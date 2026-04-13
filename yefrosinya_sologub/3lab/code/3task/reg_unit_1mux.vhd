3cf2173607b450f59f0ce197bac24503bca4f873
yefrosinya <efrosinasologub@gmail.com>
2026-04-13 12:33:41 +0300

    3,4 tasks modified

---
+++ yefrosinya_sologub/3lab/code/3task/reg_unit_1mux.vhd
@@ -0,0 +1,41 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+
+entity reg_unit is
+    generic (N: natural := 34);
+    port(
+    CLK, RST, EN: in std_logic;
+    Din: in std_logic_vector(N-1 downto 0);
+    Dout: out std_logic_vector(N-1 downto 0)
+    );
+end reg_unit;
+
+architecture Structural of reg_unit is
+    signal store: std_logic_vector(N-1 downto 0);
+    signal bit_in: std_logic_vector(N-1 downto 0);
+    signal clear_n: std_logic;
+    component lab3_task2  port (
+        D, CLK, CLR_N: in std_logic;
+        Q: out std_logic
+    );
+    end component;
+
+begin
+    clear_proc: process(CLK, RST)
+    begin
+        if rising_edge(CLK) then
+            clear_n <= not RST;
+        end if;
+    end process;
+    
+    triggers: for i in 0 to N-1 generate
+        bit_in(i) <= Din(i) when EN = '1' else store(i);
+        trig: lab3_task2 port map (
+            CLK => CLK,
+            D => bit_in(i),
+            CLR_N => clear_n,
+            Q => store(i)
+        );
+    end generate;
+    Dout <= store;
+end Structural;
--- yefrosinya_sologub/3lab/code/4task/3lab_4task.vhd
+++ yefrosinya_sologub/3lab/code/4task/3lab_4task.vhd
@@ -1,34 +1,48 @@
-library IEEE;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity freq_div_behav is
-    generic (K : natural := 10);
-    port (
-        CLK, RST, EN : in std_logic;
-        Q: out std_logic
-    );
-end freq_div_behav;
-
-architecture rtl of freq_div_behav is
-    signal counter: integer range 0 to (K/2)-1 := 0;
-    signal store: std_logic := '0';
-begin
-
-    proc: process(CLK)
-    begin
-        if rising_edge(CLK) then
-            if RST = '1' then
-                store <= '0';
-            elsif EN = '1' then
-                if counter = (K/2)-1 then
-                    counter <= 0;
-                    store <= not store; -- inv + trigger
-                else
-                    counter <= counter + 1;
-                end if;
-            end if;     
-        end if;
-   end process;
-   Q <= store;
+library IEEE; 
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.all; 
+use ieee.math_real.all; 
+ 
+entity freq_div_behav is 
+    generic (K : natural := 10); 
+    port ( 
+        CLK, RST, EN : in std_logic; 
+        Q: out std_logic 
+    ); 
+end freq_div_behav; 
+ 
+architecture rtl of freq_div_behav is 
+    constant HALF: positive := K/2; 
+    constant WIDTH: positive := integer(ceil(log2(real(HALF + 1)))); 
+    signal counter: std_logic_vector(WIDTH-1 downto 0); 
+    signal store: std_logic := '0'; 
+begin 
+ 
+    count_proc: process(CLK) 
+        begin 
+        if rising_edge(CLK) then 
+            if RST = '1' then 
+                counter <= (others => '0'); 
+            elsif EN = '1' then 
+                if unsigned(counter) = HALF-1 then 
+                    counter <= (others => '0'); 
+                else 
+                    counter <= std_logic_vector(unsigned(counter)+1); 
+                end if; 
+            end if; 
+        end if; 
+    end process; 
+ 
+    toggle_proc: process(CLK) 
+    begin 
+        if rising_edge(CLK) then 
+            if RST = '1' then 
+                store <= '0'; 
+            elsif EN = '1' and unsigned(counter) = HALF-1 then 
+                store <= not store; 
+            end if; 
+        end if; 
+    end process; 
+ 
+   Q <= store; 
 end rtl;
